75b7497f69362cb0a4aa35797e5e525e5c53f41f
PJ Kim <pj@kakao.com>
2013-04-17 09:31:12 +0900

    use jquery instead of native js syntax

--- js/effects.js
+++ js/effects.js
@@ -1,13 +1,15 @@
 /* Shake */
 
 function shakeleft(elementId) {
-  document.getElementById(elementId).style.position = "relative";
-  document.getElementById(elementId).style.left = "5px";
+  var id = '#' + elementId
+  $(id).css('position', 'relative');
+  $(id).css('left', '5px');
   timer = setTimeout("shakeright('" + elementId + "')", 10);
 }
 
 function shakeright(elementId) {
-  document.getElementById(elementId).style.left = "0";
+  var id = '#' + elementId
+  $(id).css('left', '0');
   timer = setTimeout("shakeleft('" + elementId + "')", 10);
 }
 
